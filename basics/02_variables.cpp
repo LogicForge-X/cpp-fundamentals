8516554efff23517efa7704076a84c8dde7323c4
Yashraj Gupta <logicforgex01@gmail.com>
2026-07-01 19:57:32 +0530

    Understanding and Declaring Variables

---
+++ basics/02_variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    float height = 5.9;
+    char grade = 'A';
+    string name = "Yashraj";
+    
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    
+    return 0;
+}
